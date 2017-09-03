6a0dde6c86e80e0adc5e9f52d299f9ed18bfe55e
Coleyra <masterob1@free.fr>
2017-09-03 18:42:24 +0200

    Petites correction sur FosUserBundle

--- src/BetFormulaBundle/Entity/User.php
+++ src/BetFormulaBundle/Entity/User.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  * User
  *
  * @ORM\Table(name="user")
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="BetFormulaBundle\Entity\UserRepository")
  */
 class User extends BaseUser
 {
